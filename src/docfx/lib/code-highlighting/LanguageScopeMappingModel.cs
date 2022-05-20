4d183a065dccfe74ebd4b64d01cf3290ee3992ab
Shichao Zhang <shiczha@microsoft.com>
2022-05-20 18:12:06 +0800

    lang-grammar-scope mapping

---
+++ src/docfx/lib/code-highlighting/LanguageScopeMappingModel.cs
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.Docs.Build;
+
+internal class LanguageScopeMappingModel
+{
+    public string? Scope { get; init; }
+
+    public string? Grammar { get; init; }
+}
---
+++ src/docfx/lib/code-highlighting/ScopeGrammarFileMappingModel.cs
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.Docs.Build;
+
+internal class ScopeGrammarFileMappingModel
+{
+    public string? Lang { get; init; }
+
+    public string? Scope { get; init; }
+}
